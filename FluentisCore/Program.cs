3494191f2cbf16eaab1d40e95a2a0a876630c046
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2025-05-05 18:38:27 -0600

    fix: no swagger auth

--- FluentisCore/Program.cs
+++ FluentisCore/Program.cs
@@ -51,46 +51,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     }, options => { });
 builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.OAuth2,
-        Flows = new OpenApiOAuthFlows
-        {
-            AuthorizationCode = new OpenApiOAuthFlow
-            {
-                AuthorizationUrl = new Uri("https://login.microsoftonline.com/846e3824-7539-4a0d-bfb6-00745fba3165/oauth2/v2.0/authorize"),
-                TokenUrl = new Uri("https://login.microsoftonline.com/846e3824-7539-4a0d-bfb6-00745fba3165/oauth2/v2.0/token"),
-                Scopes = new Dictionary<string, string>
-                {
-                    { "https://asofarmabpm.onmicrosoft.com/badd1a2d-8427-4f00-b56d-ddbbd9f1883e/access_as_user", "Access API as user" },
-                },
-            },
-        },
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" },
-            },
-            new[] { "https://asofarmabpm.onmicrosoft.com/badd1a2d-8427-4f00-b56d-ddbbd9f1883e/access_as_user" }
-        },
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.OAuthClientId("cbd0adaf-be24-4605-942b-d64ace0763a7"); //cliend id de frontend
-    c.OAuthScopes("https://asofarmabpm.onmicrosoft.com/badd1a2d-8427-4f00-b56d-ddbbd9f1883e/access_as_user");
-    c.OAuthUsePkce();
-});
-
+app.UseSwaggerUI();
 app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
 app.UseAuthentication();
