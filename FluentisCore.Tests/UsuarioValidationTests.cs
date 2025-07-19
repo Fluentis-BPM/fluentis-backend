f8b3c4819ba43eef4d135774c27f7067cb40cee4
skemono <her231038@uvg.edu.gt>
2025-07-18 20:25:19 -0600

    feat: Unit testing and Relacion Visualizador

---
+++ FluentisCore.Tests/UsuarioValidationTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentisCore.Models.UserManagement;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace FluentisCore.Tests
+{
+    public class UsuarioValidationTests
+    {
+        [Fact]
+        public void UsuarioEmailIsRequired()
+        {
+            var usuario = new Usuario
+            {
+                Nombre = "NoEmail"
+                // Email is missing
+            };
+
+            var context = new ValidationContext(usuario, null, null);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(usuario, context, results, true);
+
+            Assert.False(isValid);
+            Assert.Contains(results, r => r.MemberNames.Contains("Email"));
+        }
+    }
+
+}
