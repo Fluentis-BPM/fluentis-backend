9f83a4c3f879bed30b29821ddb293ddc31e28d0a
skemono <her231038@uvg.edu.gt>
2025-08-11 18:03:28 -0600

    refactor: moved GetTipoFlujo to new service interface

---
+++ FluentisCore/Services/WorkflowManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using FluentisCore.Models.WorkflowManagement;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentisCore.Services
+{
+    public interface IWorkflowService
+    {
+        Task<TipoFlujo> GetTipoFlujo(int pasoId, DbContext context);
+    }
+
+    public class WorkflowService : IWorkflowService
+    {
+        public async Task<TipoFlujo> GetTipoFlujo(int pasoId, DbContext context)
+        {
+            var destinos = await context.Set<CaminoParalelo>()
+                .Where(c => c.PasoOrigenId == pasoId)
+                .Select(c => c.PasoDestinoId)
+                .ToListAsync();
+
+            var origenes = await context.Set<CaminoParalelo>()
+                .Where(c => c.PasoDestinoId == pasoId)
+                .Select(c => c.PasoOrigenId)
+                .ToListAsync();
+
+            if (destinos.Count > 1) return TipoFlujo.Bifurcacion;
+            if (origenes.Count > 1) return TipoFlujo.Union;
+            return TipoFlujo.Normal;
+        }
+    }
+}
